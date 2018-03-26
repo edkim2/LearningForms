881d6c51882cc7e9c47d243506875c123deabd0c
edkim2 <ekim10203@gmail.com>
2018-03-26 10:25:25 -0500

    Added confirmation messages

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -59,6 +59,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully added!";
+
                 return RedirectToAction("Index");
             }
             SetUpActivitiesSelectListItems();
@@ -102,6 +104,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.UpdateEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully updated!";
+
                 return RedirectToAction("Index");
             }
 
@@ -137,6 +141,8 @@ namespace Treehouse.FitnessFrog.Controllers
         {
             // TODO Delete the entry
             _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was successfully deleted!";
 
             // TODO Redirect to the "Entries" list page
             return RedirectToAction("Index");
